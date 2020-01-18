d9b11e8b4fad871593295b162bac2b5bf40b2f65
Brian Utterback <brian@utterback.org>
2020-01-17 20:58:07 -0500

    Add splash screen logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,9 +7,17 @@
 
 package frc.robot;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -28,6 +36,19 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+
+    // Print the Splash Screen
+    System.out.println("==============================================");
+    System.out.println("Starting robotInit for Tough Techs");
+    printStatusFiles("deployhost.txt", true, 0, 2, 1);
+    printStatusFiles("deploytime.txt", true, 0, 3, 2);
+    printStatusFiles("buildtime.txt", false, 0, 0, 2);
+    printStatusFiles("branch.txt", false, 0, 5, 1);
+    printStatusFiles("commit.txt", false, 1, 0, 10);
+    printStatusFiles("changes.txt", false, 2, 0, 10);
+    printStatusFiles("remote.txt", false, 3, 0, 10);
+    System.out.println("============================================");
+
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
@@ -109,5 +130,39 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testPeriodic() {
+  }
+
+  private void printStatusFiles(String filename, Boolean isDeploy, int rowIndex, int colIndex, int widthIndex) {
+    byte[] buffer = new byte[1024];
+    InputStream statusfile;
+    try {
+      if (isDeploy) {
+        if (RobotBase.isSimulation()) {
+          statusfile = new BufferedInputStream(
+              new FileInputStream(Filesystem.getLaunchDirectory() + "/src/main/deploy/" + filename));
+        } else {
+          statusfile = new BufferedInputStream(new FileInputStream(Filesystem.getDeployDirectory() + "/" + filename));
+        }
+      } else {
+        statusfile = getClass().getResourceAsStream("/" + filename);
+      }
+      System.out.print((filename + ": ").replace(".txt", ""));
+      try {
+        for (int length = 0; (length = statusfile.read(buffer)) != -1;) {
+          String buf = new String(buffer).replaceAll("\\s"," ");
+          String tfn = filename.replace(".txt", "");
+          String fn = tfn.substring(0,1).toUpperCase() + tfn.substring(1);
+          System.out.write(buffer, 0, length);
+          SmartDashboard.putString(fn, buf);
+          Shuffleboard.getTab("Status").add(fn, buf).withPosition(colIndex, rowIndex).withSize(widthIndex, 1);
+        }
+      } finally {
+        System.out.println();
+        statusfile.close();
+      }
+    } catch (Exception e) {
+      System.out.println("Unable to find file.");
+      System.out.println(e.getMessage());
+    }
   }
 }
