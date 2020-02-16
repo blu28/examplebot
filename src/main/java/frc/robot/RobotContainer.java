bf9b3c4a33dbc24e9bbd09911da776900484d88b
Brian Utterback <brian@utterback.org>
2020-02-16 17:57:47 -0500

    Get rid of example classes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,8 +9,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.DriveWithJoysticksCommand;
+import frc.robot.subsystems.DriveSubsystem;
 import io.github.oblarg.oblog.Logger;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -22,19 +22,17 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
+  private final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
+  private final DriveWithJoysticksCommand m_driveCommand = new DriveWithJoysticksCommand(m_driveSubsystem);
 
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
+    Logger.configureLoggingAndConfig(this, false);
     // Configure the button bindings
     configureButtonBindings();
-    Logger.configureLoggingAndConfig(this, false);
   }
 
   /**
@@ -54,6 +52,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return m_driveCommand;
   }
 }
